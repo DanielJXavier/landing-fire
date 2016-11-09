7ef3de1a16e69539beb6bc7cf0bb87479c7e4d00
Daniel Xavier <dxdanielxavier@gmail.com>
2016-11-09 03:53:52 -0200

    Owl Carousel (Testimonies)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -19,6 +19,40 @@ function onPlayerStateChange() {
 
 // jQuery
 $(function() {
+	// Owl Carousel
+	var owl = $(".fire-testimonies-carousel");
+	owl.owlCarousel({singleItem: true, mouseDrag: false});
+	function setItemsCount() {
+		$("#items-count").text(("0" + currentItem).slice(-2) + " de " + ("0" + totalItems).slice(-2));
+		if (currentItem == totalItems) {
+			$("#next-item").removeClass("enabled").addClass("disabled");
+		} else {
+			$("#next-item").removeClass("disabled").addClass("enabled");
+		}
+		if (currentItem == 1) {
+			$("#prev-item").removeClass("enabled").addClass("disabled");
+		} else {
+			$("#prev-item").removeClass("disabled").addClass("enabled");
+		}
+	}
+	var currentItem = 1;
+	var totalItems = $(".fire-testimony").length;
+	setItemsCount();
+	$("#prev-item").click(function() {
+		if (currentItem > 1) {
+			currentItem--;
+			owl.trigger('owl.prev');
+			setItemsCount();
+		}
+	});
+	$("#next-item").click(function() {
+		if (currentItem < totalItems) {
+			currentItem++;
+			owl.trigger('owl.next');
+			setItemsCount();
+		}
+	});
+
 	// Video Player
 	$('.fire-video-play').click(function(){
 		player.playVideo();
